ac996bc0b6ce2e97b5a281a26885f89d32c190e5
andrewt0301 <andrewt0301@gmail.com>
2019-04-24 09:52:57 +0300

    Added Python example
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

---
+++ tutorials/workshop01/python/plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import csv
+
+x  = []
+y1 = []
+y2 = []
+
+with open('data.csv', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x.append(int(row[0]))
+        y1.append(int(row[1]))
+        y2.append(int(row[2]))
+
+plt.plot(x, y1, label='First algorithm')
+plt.plot(x, y2, label='Second algorithm')
+
+plt.xlabel('Size (N)')
+plt.ylabel('Time (milliseconds)')
+plt.title('Performance of different algorithms')
+plt.legend()
+plt.show()
