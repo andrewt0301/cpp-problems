13d5cb8b9841eba3162c0aaa7f31772bb296d864
andrewt0301 <andrewt0301@gmail.com>
2019-04-29 19:17:14 +0300

    Small update
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- tutorials/workshops/workshop01/workshop01.cpp
+++ tutorials/workshops/workshop01/workshop01.cpp
@@ -9,39 +9,14 @@
 #include <iterator>
 #include <vector>
 
+#include "../Random.h"
 #include "../Time.h"
 
 #include "Multiplicator.h"
 #include "Number.h"
-#include "../Random.h"
-
-using IntVector = std::vector<int>;
-
-using Sample = IntVector;
-
-void generateCsv(const std::vector<Sample>& samples)
-{
-    std::ofstream out{"test.csv", std::ofstream::out};
-
-    for (const Sample& sample : samples)
-    {
-        std::copy(sample.cbegin(), sample.cend(), std::ostream_iterator<int>(out, ";"));
-        out << std::endl;
-    }
-
-    out.close();
-}
 
 int main()
 {
-
-    std::vector<Sample> samples;
-
-    samples.push_back({1, 2, 3});
-    samples.push_back({2, 4, 5});
-    samples.push_back({3, 6, 7});
-
-    generateCsv(samples);
 
     return 0;
 }
