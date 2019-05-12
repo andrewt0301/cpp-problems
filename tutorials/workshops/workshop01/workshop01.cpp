ec8b5ddd2eb2d6eb3b00e5c528915fe9156ae066
Andrei Tatarnikov <andrewt0301@gmail.com>
2019-05-12 18:27:09 +0300

    Updated
    Signed-off-by: Andrei Tatarnikov <andrewt0301@gmail.com>

--- tutorials/workshops/workshop01/workshop01.cpp
+++ tutorials/workshops/workshop01/workshop01.cpp
@@ -9,6 +9,7 @@
 #include <iterator>
 #include <vector>
 
+#include "../CSV.h"
 #include "../Random.h"
 #include "../Time.h"
 
@@ -21,15 +22,69 @@
 using std::cout;
 using std::endl;
 
+using NumberVector = std::vector<Number>;
+
+Number randomNumber(Random& random, size_t length)
+{
+    std::vector<int> data = randomVector(random, length);
+    Number::Digits digits(length);
+
+    for (size_t i = 0; i < length; ++i)
+        digits[i] = data[i];
+
+    return Number{digits, random.next() % 2 == 0};
+}
+
+NumberVector randomNumberVector(Random& random, size_t length, size_t count)
+{
+    NumberVector numbers{count};
+
+    for (Number& number : numbers)
+        number = randomNumber(random, length);
+
+    return numbers;
+}
+
 int main()
 {
+    // Check than the implementation is correct.
     runNumberTests();
     runMultiplicatorTests();
 
-    Number x{"6666"};
-    Number y{"6666"};
+    // Here hold our performance measures.
+    Samples<long> samples;
 
-    cout << Multiplicator::karatsuba(x,y) << endl;
+    // This Random generates digits.
+    Random random{1};
+
+    for (size_t length = 1; length <= 128; ++length)
+    {
+        NumberVector testData = randomNumberVector(random, length, 20 * 2);
+
+        long gradeSchoolTime = calcExecTime([testData]()
+        {
+            for (size_t index = 0; index < testData.size() / 2; ++index)
+                Multiplicator::gradeSchool(testData[index], testData[index * 2]);
+        });
+
+        long divideAndConquerTime = calcExecTime([testData]()
+        {
+            for (size_t index = 0; index < testData.size() / 2; ++index)
+                Multiplicator::divideAndConquer(testData[index], testData[index * 2]);
+        });
+
+        long karatsubaTime = calcExecTime([testData]()
+        {
+            for (size_t index = 0; index < testData.size() / 2; ++index)
+                Multiplicator::karatsuba(testData[index], testData[index * 2]);
+        });
+
+        // We add a sample to the samples vector.
+        samples.push_back({static_cast<long>(length), gradeSchoolTime, divideAndConquerTime, karatsubaTime});
+    }
+
+    // We save samples to a CSV file.
+    generateCSV("number_mult.csv", samples);
 
     return 0;
 }
