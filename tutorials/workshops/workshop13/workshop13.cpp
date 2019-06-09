427a7fb44e88efd1833a25f4c2391a025be01748
Andrei Tatarnikov <andrewt0301@gmail.com>
2019-06-09 15:39:40 +0300

    Updated.
    Signed-off-by: Andrei Tatarnikov <andrewt0301@gmail.com>

--- tutorials/workshops/workshop13/workshop13.cpp
+++ tutorials/workshops/workshop13/workshop13.cpp
@@ -9,31 +9,41 @@
 
 #include <iostream>
 
-int main()
+template <typename TGraph>
+void testBfsDfs()
 {
-    using Graph = GraphMultimap<int>;
+    using Graph = TGraph;
     using Node  = Node<int>;
 
     Graph graph;
 
     Node* node1 = graph.addNode(1);
-    Node* node2 = graph.addNode(6);
+    Node* node2 = graph.addNode(2);
     Node* node3 = graph.addNode(3);
     Node* node4 = graph.addNode(4);
     Node* node5 = graph.addNode(5);
+    Node* node6 = graph.addNode(6);
+    Node* node7 = graph.addNode(7);
+    Node* node8 = graph.addNode(8);
 
     graph.addEdge(node1, node2);
     graph.addEdge(node1, node3);
-    graph.addEdge(node4, node3);
-    graph.addEdge(node3, node5);
+    graph.addEdge(node3, node4);
+    graph.addEdge(node4, node5);
+    graph.addEdge(node5, node8);
+    graph.addEdge(node6, node7);
+    graph.addEdge(node7, node2);
+    graph.addEdge(node8, node6);
 
     std::cout << graph;
-    std::cout << std::endl << "***************" << std::endl;
+    std::cout << std::endl << "*************** Remove *************" << std::endl;
 
-    graph.removeNode(node4);
-    graph.removeEdge(node1, node2);
+    graph.removeEdge(node7, node2);
+    graph.removeNode(node7);
 
-    std::cout << std::endl << "*************** BST: " << std::endl;
+    std::cout << graph;
+
+    std::cout << std::endl << "*************** BFS ***************:" << std::endl;
 
     bfs(graph, node1, [](Node* node, BfsVertex<int>* vertex)
     {
@@ -50,7 +60,7 @@ int main()
         std::cout << std::endl;
     });
 
-    std::cout << std::endl << "*************** DST: " << std::endl;
+    std::cout << std::endl << "*************** DFS ***************:" << std::endl;
 
     dfs(graph, node1, [](Node* node, DfsVertex<int>* vertex)
     {
@@ -67,27 +77,13 @@ int main()
 
         std::cout << std::endl;
     });
+}
 
-    std::cout << std::endl << "***************" << std::endl;
+int main()
+{
 
-    std::cout << graph;
-    std::cout << std::endl << "***************" << std::endl;
-    std::cout << graph;
-    std::cout << std::endl << "***************" << std::endl;
-
-    auto r = graph.getNodes();
-    for (auto it = r.first; it != r.second; ++it)
-    {
-        std::cout << (*it)->tag << std::endl;
-    }
-
-    std::cout << std::endl << "***************" << std::endl;
-
-    auto r1 = graph.getEdges(node1);
-    for (auto it = r1.first; it != r1.second; ++it)
-    {
-           std::cout << (*it)->tag << std::endl;
-    }
+    testBfsDfs<GraphMultimap<int>>();
+    //testBfsDfs<GraphMap<int>>();
 
     return 0;
 }
