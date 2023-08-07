91fb58a96718a38236bf91c8854853b278abdef9
Andrei Tatarnikov <andrewt0301@gmail.com>
2023-08-07 15:25:29 +0300

    Updated RB Tree
    Signed-off-by: Andrei Tatarnikov <andrewt0301@gmail.com>

--- tutorials/workshops/workshop10/RedBlackTree.h
+++ tutorials/workshops/workshop10/RedBlackTree.h
@@ -6,245 +6,198 @@
 #define TUTORIALS_REDBLACKTREE_H
 
 template <typename T>
-class RedBlackTree
-{
+class RedBlackTree {
 public:
-
-    enum Color
-    {
-        RED,
-        BLACK
-    };
+    enum Color { RED, BLACK };
+    enum Walk  { PRE, IN, POST };
 
     struct Node;
     static Node* const NIL;
 
-    struct Node
-    {
+    struct Node {
         Node* p     = NIL;
         Node* left  = NIL;
         Node* right = NIL;
         Color color = BLACK;
         T     key;
+
+        explicit Node(T k = T(), Color c = BLACK)
+            : key(k), color(c) {}
     };
 
     RedBlackTree() = default;
 
-    ~RedBlackTree()
-    {
-        postorderWalk(_root, [](Node* node)
-        {
-            delete node;
-        });
-     }
+    ~RedBlackTree() {
+        visit<POST>(_root, [](Node* node) { delete node; });
+    }
 
     RedBlackTree(const RedBlackTree&) = delete;
     RedBlackTree& operator=(const RedBlackTree&) = delete;
 
-    bool isEmpty() const
-    {
+    bool isEmpty() const {
         return _root == NIL;
     }
 
-    size_t size() const
-    {
+    size_t size() const {
         return _size;
     }
 
-    Node* min() const
-    {
+    Node* min() const {
         return min(_root);
     }
 
-    static Node* min(Node* node)
-    {
-        while (node->left != NIL)
+    static Node* min(Node* node) {
+        while (node->left != NIL) {
             node = node->left;
+        }
         return node;
     }
 
-    static Node* successor(Node* node)
-    {
-        if (node->right != NIL)
+    static Node* successor(Node* node) {
+        if (node->right != NIL) {
             return min(node->right);
-
+        }
         Node* parent = node->p;
-        while (parent != NIL && node == parent->right)
-        {
+        while (parent != NIL && node == parent->right) {
             node = parent;
             parent = node->p;
         }
-
         return parent;
     }
 
-    Node* max() const
-    {
+    Node* max() const {
         return max(_root);
     }
 
-    static Node* max(Node* node)
-    {
-        while (node->right != NIL)
+    static Node* max(Node* node) {
+        while (node->right != NIL) {
             node = node->right;
+        }
         return node;
     }
 
-    static Node* predecessor(Node* node)
-    {
-        if (node->left != NIL)
+    static Node* predecessor(Node* node) {
+        if (node->left != NIL) {
             return max(node->left);
-
+        }
         Node* parent = node->p;
-        while (parent != NIL && node == parent->left)
-        {
+        while (parent != NIL && node == parent->left) {
             node = parent;
             parent = node->p;
         }
-
         return parent;
     }
 
-    Node* search(T key)
-    {
+    Node* search(T key) {
         Node* node = _root;
-        while (node != NIL && node->key != key)
-        {
-            if (key < node->key)
-                node = node->left;
-            else
-                node = node->right;
+        while (node != NIL && node->key != key) {
+            node = key < node->key ? node->left : node->right;
         }
         return node;
     }
 
-    void insert(T key)
-    {
-        Node* node = new Node();
-        node->key = key;
-
+    void insert(T key) {
+        Node* node = new Node(key);
         insert(node);
-        _size++;
+        ++_size;
     }
 
-    void insert(Node* newNode)
-    {
+    void insert(Node* newNode) {
         Node* parent = NIL;
         Node* node = _root;
-
-        while (node != NIL)
-        {
+        while (node != NIL) {
             parent = node;
-            if (newNode->key < node->key)
-                node = node->left;
-            else
-                node = node->right;
+            node = newNode->key < node->key ? node->left : node->right;
         }
-
         newNode->p = parent;
-        if (parent == NIL)
+        if (parent == NIL) {
             _root = newNode;
-        else if (newNode->key < parent->key)
+        } else if (newNode->key < parent->key) {
             parent->left = newNode;
-        else
+        } else {
             parent->right = newNode;
+        }
     }
 
-    void transplant(Node* oldNode, Node* newNode)
-    {
-        if (oldNode->p == NIL)
+    void transplant(Node* oldNode, Node* newNode) {
+        if (oldNode->p == NIL) {
             _root = newNode;
-        else if (oldNode == oldNode->p->left)
+        } else if (oldNode == oldNode->p->left) {
             oldNode->p->left = newNode;
-        else
+        } else {
             oldNode->p->right = newNode;
-
+        }
         newNode->p = oldNode->p;
     }
 
-    void remove(T key)
-    {
+    void remove(T key) {
         Node* node = search(key);
-        if (node != NIL)
-        {
+        if (node != NIL) {
             remove(node);
             delete node;
-            _size--;
+            --_size;
         }
     }
 
-    void remove(Node* node)
-    {
-        if (node->left == NIL)
+    void remove(Node* node) {
+        if (node->left == NIL) {
             transplant(node, node->right);
-        else if (node->right == NIL)
+        } else if (node->right == NIL) {
             transplant(node, node->left);
-        else
-        {
-            Node* sucessor = min(node->right);
-            if (sucessor->p != node)
-            {
-                transplant(sucessor, sucessor->right);
-                sucessor->right = node->right;
-                sucessor->right->p = sucessor;
+        } else {
+            Node* successor = min(node->right);
+            if (successor->p != node) {
+                transplant(successor, successor->right);
+                successor->right = node->right;
+                successor->right->p = successor;
             }
-            transplant(node, sucessor);
-            sucessor->left = node->left;
-            sucessor->left->p = sucessor;
+            transplant(node, successor);
+            successor->left = node->left;
+            successor->left->p = successor;
         }
     }
 
-    void leftRotate(Node* x)
-    {
+    void leftRotate(Node* x) {
         Node* y = x->right;
         x->right = y->left;
-
-        if (y->left != NIL)
+        if (y->left != NIL) {
             y->left->p = x;
-
+        }
         y->p = x->p;
-        if (x->p == NIL)
+        if (x->p == NIL) {
             _root = y;
-        else if (x == x->p->left)
+        } else if (x == x->p->left) {
             x->p->left = y;
-        else
+        } else {
             x->p->right = y;
-
+        }
         y->left = x;
         x->p = y;
     }
 
-    void rightRotate(Node* x)
-    {
+    void rightRotate(Node* x) {
         Node* y = x->left;
         x->left = y->right;
-
-        if (y->right != NIL)
+        if (y->right != NIL) {
             y->right->p = x;
-
+        }
         y->p = x->p;
-        if (x->p == NIL)
+        if (x->p == NIL) {
             _root = y;
-        else if (x == x->p->left)
+        } else if (x == x->p->left) {
             x->p->left = y;
-        else
+        } else {
             x->p->right = y;
-
+        }
         y->right = x;
         x->p = y;
     }
 
-    void insertRB(T key)
-    {
-        Node* node = new Node();
-
-        node->key = key;
-        node->color = RED;
-
+    void insertRB(T key) {
+        Node* node = new Node(key, RED);
         insert(node);
         fixupInsertRB(node);
-
-        _size++;
+        ++_size;
     }
 
     void fixupInsertRB(Node* z)
@@ -311,47 +264,34 @@ public:
         _root->color = BLACK;
     }
 
-    void removeRB(Node* node)
-    {
+    void removeRB(Node* node) {
         Color origColor = node->color;
-        Node* trans = NIL;
-
-        if (node->left == NIL)
-        {
+        Node* trans;
+        if (node->left == NIL) {
             trans = node->right;
             transplant(node, trans);
-        }
-        else if (node->right == NIL)
-        {
+        } else if (node->right == NIL) {
             trans = node->left;
             transplant(node, trans);
-        }
-        else
-        {
+        } else {
             Node* successor = min(node->right);
-
             origColor = successor->color;
             trans = successor->right;
-
-            if (successor->p == node)
-            {
+            if (successor->p == node) {
                 trans->p = successor;
-            }
-            else
-            {
+            } else {
                 transplant(successor, trans);
                 successor->right = node->right;
                 successor->right->p = successor;
             }
-
             transplant(node, successor);
             successor->left = node->left;
             successor->left->p = successor;
             successor->color = node->color;
         }
-
-        if (origColor == BLACK)
+        if (origColor == BLACK) {
             fixupRemoveRB(trans);
+        }
     }
 
     void fixupRemoveRB(Node* x)
@@ -436,78 +376,41 @@ public:
         x->color = BLACK;
     }
 
-    template<typename TAct>
-    static void preorderWalk(Node* node, TAct action)
-    {
-        if (node != NIL)
-        {
-            action(node);
-            preorderWalk(node->left, action);
-            preorderWalk(node->right, action);
+    template<Walk TWalk, typename TAct>
+    void visit(TAct action) const {
+        visit<TWalk>(_root, action);
+    }
+
+    template<Walk TWalk, typename TAct>
+    static void visit(Node* node, TAct action) {
+        if (node != NIL) {
+            if (TWalk == PRE) {
+                action(node);
+            }
+            visit<TWalk>(node->left, action);
+            if (TWalk == IN) {
+                action(node);
+            }
+            visit<TWalk>(node->right, action);
+            if (TWalk == POST) {
+                action(node);
+            }
         }
     }
 
-    template<typename TAct>
-    static void inorderWalk(Node* node, TAct action)
-    {
-        if (node != NIL)
-        {
-            inorderWalk(node->left, action);
-            action(node);
-            inorderWalk(node->right, action);
-        }
-    }
-
-    template<typename TAct>
-    static void postorderWalk(Node* node, TAct action)
-    {
-        if (node != NIL)
-        {
-            postorderWalk(node->left, action);
-            postorderWalk(node->right, action);
-            action(node);
-        }
-    }
-
-    void printAsRBTree(std::ostream& out)
-    {
+    void printAsRBTree(std::ostream& out) {
         printAsRBTree(out, _root, 0);
     }
 
-    static void printAsRBTree(std::ostream& out, Node* node, size_t depth)
-    {
-        if (node == NIL)
+    static void printAsRBTree(std::ostream& out, const Node* node, size_t depth) {
+        if (node == NIL) {
             return;
-
+        }
         printAsRBTree(out, node->right, depth + 1);
-
-        out << "\r\n";
-        for (size_t i = 0; i < depth; ++i)
-            out << "     ";
-
-        out << node->key;
-        out << "(";
-        out << (node->color == RED ? 'R' : 'B');
-        out << ")";
-
+        out << std::endl;
+        std::fill_n(std::ostreambuf_iterator<char>(out), depth * 4, ' ');
+        out << node->key << "(" << (node->color == RED ? 'R' : 'B') << ")";
         printAsRBTree(out, node->left,  depth + 1);
-    }
-
-    template <typename U>
-    friend std::ostream& operator<<(std::ostream& out, const RedBlackTree<U>& tree)
-    {
-        out << '{';
-        bool first = true;
-        RedBlackTree<U>::inorderWalk(tree._root, [&out, &first](Node* node)
-        {
-            if (!first)
-                out << ", ";
-            else
-                first = false;
-            out << node->key;
-        });
-        out << '}';
-        return out;
     }
 
 private:
@@ -517,5 +420,21 @@ private:
 
 template <typename T>
 typename RedBlackTree<T>::Node* const RedBlackTree<T>::NIL = new typename RedBlackTree<T>::Node();
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const RedBlackTree<T>& tree) {
+    out << '{';
+    bool first = true;
+    tree.template visit<RedBlackTree<T>::IN>([&out, &first](typename RedBlackTree<T>::Node* node) {
+        if (!first) {
+            out << ", ";
+        } else {
+            first = false;
+        }
+        out << node->key;
+    });
+    out << '}';
+    return out;
+}
 
 #endif //TUTORIALS_REDBLACKTREE_H
